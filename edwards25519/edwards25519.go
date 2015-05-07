4d95a2b72782c4564b18c969f7f03f148c2a4141
Donovan Hide <donovanhide@gmail.com>
2015-05-07 18:14:20 +0100

    Simplify and speed up conditional move

--- edwards25519/edwards25519.go
+++ edwards25519/edwards25519.go
@@ -69,17 +69,9 @@ func FeCopy(dst, src *FieldElement) {
 //
 // Preconditions: b in {0,1}.
 func FeCMove(f, g *FieldElement, b int32) {
-	b = -b
-	f[0] ^= b & (f[0] ^ g[0])
-	f[1] ^= b & (f[1] ^ g[1])
-	f[2] ^= b & (f[2] ^ g[2])
-	f[3] ^= b & (f[3] ^ g[3])
-	f[4] ^= b & (f[4] ^ g[4])
-	f[5] ^= b & (f[5] ^ g[5])
-	f[6] ^= b & (f[6] ^ g[6])
-	f[7] ^= b & (f[7] ^ g[7])
-	f[8] ^= b & (f[8] ^ g[8])
-	f[9] ^= b & (f[9] ^ g[9])
+	if b == 1 {
+		copy(f[:], g[:])
+	}
 }
 
 func load3(in []byte) int64 {
